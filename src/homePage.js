4e9939e71317967aaa2b2d5f6a44263914d1b857
chen <chenlipy@gmail.com>
2024-02-09 16:18:10 +0200

    added menu

--- src/homePage.js
+++ src/homePage.js
@@ -6,61 +6,54 @@ class ApartmentManager {
         this.savedApartments = document.getElementById('savedApartments');
         this.sort = document.getElementById('sort');
         this.filter = document.getElementById('filter');
-        this.menuButton = document.getElementById('menuButton');
-        this.sidebar = document.getElementById('sidebar');
-        this.advancedFilterButton = document.getElementById('advancedFilterButton'); // Add reference to the advanced filter button
+        this.filterBar = document.getElementById('filter-bar');
+        this.advancedFilterButton = document.getElementById('advancedFilterButton');
 
-        // this.advancedFilterButtons.addEventListener('click', this.toggleSidebar.bind(this)); // Add event listener for advanced filter button
-
-        // Add event listeners
-        // this.menuButton.addEventListener('mouseenter', this.openSidebar.bind(this));
-        this.sidebar.addEventListener('submit', this.applyFilters.bind(this));
+        // Event listeners
+        this.filterBar.addEventListener('submit', this.applyFilters.bind(this));
         this.advancedFilterButton.addEventListener('click', this.addRatingFilter.bind(this));
 
-        // Add event listeners
-        // this.menuButton.addEventListener('mouseenter', this.openSidebar.bind(this));
 
         window.onload = this.displayApartments.bind(this);
     }
 
 
     addRatingFilter() {
-        // Create label element for rating
-        const ratingLabel = document.createElement('label');
-        ratingLabel.setAttribute('for', 'rating');
-        ratingLabel.textContent = 'Rating:';
+        // Check if the rating input already exists
+        if (!document.getElementById('rating')) {
+            // Create label element for rating
+            const ratingLabel = document.createElement('label');
+            ratingLabel.setAttribute('for', 'rating');
+            ratingLabel.textContent = 'Rating:';
 
-        // Create input element for rating
-        const ratingInput = document.createElement('input');
-        ratingInput.setAttribute('type', 'number');
-        ratingInput.setAttribute('id', 'rating');
-        ratingInput.setAttribute('name', 'rating');
-        ratingInput.setAttribute('min', '0');
-        ratingInput.setAttribute('step', '0.1'); // Specify step attribute to allow decimal values
+            // Create input element for rating
+            const ratingInput = document.createElement('input');
+            ratingInput.setAttribute('type', 'number');
+            ratingInput.setAttribute('id', 'rating');
+            ratingInput.setAttribute('name', 'rating');
+            ratingInput.setAttribute('min', '0');
+            ratingInput.setAttribute('step', '0.1'); // Specify step attribute to allow decimal values
 
-        // Insert the label and input before the buttons
-        const filterForm = document.getElementById('filterForm');
-        filterForm.insertBefore(ratingInput, filterForm.querySelector('button[type="submit"]'));
-        filterForm.insertBefore(ratingLabel, ratingInput);
+            // Insert the label and input before the buttons
+            const filterForm = document.getElementById('filterForm');
+            filterForm.insertBefore(ratingInput, filterForm.querySelector('button[type="submit"]'));
+            filterForm.insertBefore(ratingLabel, ratingInput);
+
+            this.closeAdvancedFilter();
+
+        }
+
     }
-    // Other methods...
-
-
     // Function to display all apartments
     async displayApartments() {
         try {
-            // Fetch apartments from the server
             const response = await fetch('http://localhost:63341/Apartments');
             const apartments = await response.json();
 
-            // Check if there are any apartments
             if (apartments.length > 0) {
-                // Get the container element where you want to display the apartments
                 const content = document.querySelector('.content');
 
-                // Loop through each apartment
                 apartments.forEach(apartment => {
-                    // Create a div element for each apartment
                     const apartmentBox = document.createElement('div');
                     apartmentBox.classList.add('apartment-box');
 
@@ -138,7 +131,7 @@ class ApartmentManager {
     }
 
     // Function to render filtered apartments based on filter criteria
-    async renderFilteredApartments(location, minPrice, maxPricem, rating) {
+    async renderFilteredApartments(location, minPrice, maxPrice, rating) {
         try {
             const url = new URL('http://localhost:63341/Apartments');
             console.log("render fileted");
@@ -153,38 +146,28 @@ class ApartmentManager {
             const response = await fetch(url.toString());
             const apartments = await response.json();
 
-            // Filter apartments based on the filter criteria
+            // Filter apartments
             const filteredApartments = apartments.filter(apartment => {
-                // Apply location filter
                 if (params.location && apartment.location.toLowerCase().indexOf(params.location.toLowerCase()) === -1) {
                     return false;
                 }
-                // Apply minimum price filter
                 if (params.minPrice && apartment.pricePerNight < params.minPrice) {
                     return false;
                 }
-                // Apply maximum price filter
                 if (params.maxPrice && apartment.pricePerNight > params.maxPrice) {
                     return false;
                 }
-                // Apply rating filter
                 if (params.rating && apartment.avgRate < params.rating) {
                     return false;
                 }
                 return true;
             });
 
-
-            console.log(filteredApartments);
-
-
             // Get the container element where you want to display the apartments
             const content = document.querySelector('.content');
             content.innerHTML = ''; // Clear existing content
 
-            // Check if there are any apartments
             if (filteredApartments.length > 0) {
-                // Loop through each apartment
                 filteredApartments.forEach(apartment => {
                     // Create a div element for each apartment
                     const apartmentBox = document.createElement('div');
@@ -226,35 +209,31 @@ class ApartmentManager {
     applyFilters(event) {
         event.preventDefault();
         const location = document.getElementById('location').value;
+        console.log(location);
         const minPrice = parseFloat(document.getElementById('minPrice').value);
         const maxPrice = parseFloat(document.getElementById('maxPrice').value);
-        const rating = parseFloat(document.getElementById('rating').value);
+        let rating = 0; // Default rating value is 0
+
+        // Check if the rating input element exists
+        const ratingInput = document.getElementById('rating');
+        if (ratingInput) {
+            rating = parseFloat(ratingInput.value);
+        }
 
         console.log(location);
         console.log(minPrice);
         console.log(maxPrice);
         console.log(rating);
 
-        // console.log(location);
-        // // Call a function to fetch apartments based on the provided filters
+        // Call a function to fetch apartments based on the provided filters
         this.renderFilteredApartments(location, minPrice, maxPrice, rating);
     }
 
-    // Function to apply filters and fetch data
-
-    // Function to fetch apartments based on filters
 
 
-    // Function to open the sidebar menu
-    openSidebar() {
-        this.sidebar.style.width = '250px'; // Set sidebar width to show menu
-        this.menuButton.style.display = 'none'; // Hide menu button
-    }
 
-    // Function to close the sidebar menu
-    closeSidebar() {
-        this.sidebar.style.width = '0'; // Set sidebar width to hide menu
-        this.menuButton.style.display = 'block'; // Show menu button
+    closeAdvancedFilter() {
+        this.advancedFilterButton.style.display = 'none';
     }
 }
 
