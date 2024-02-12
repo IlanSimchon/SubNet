19fb102a81b8e93d004674c848ebb262776a1db1
chen <chenlipy@gmail.com>
2024-02-12 13:46:18 +0200

    updated contact css and html and login content

--- src/apartment.js
+++ src/apartment.js
@@ -22,19 +22,46 @@ export class Apartment {
         }
     }
 
-    displayApartmentDetails(apartmentDetails) {
-        // Construct the query parameters string
-        const queryParams = new URLSearchParams({
-            location: apartmentDetails.location,
-            pricePerNight: apartmentDetails.pricePerNight,
-            availability: JSON.stringify(apartmentDetails.availability),
-            reviews: apartmentDetails.reviews,
-            avgRate: apartmentDetails.avgRate,
-            photo: apartmentDetails.photo,
-            connectionDetails: apartmentDetails.connectionDetails
-        });
+    async fetchApartmentImage() {
+        try {
+            // Fetch the apartment image using the apartmentId
+            const imageResponse = await fetch(`http://localhost:63341/getApartmentPic/${this.apartmentId}`);
 
-        // Redirect to apartment.html with query parameters
-        window.location.href = `apartment.html?${queryParams.toString()}`;
+            if (imageResponse.ok) {
+                // If there's a picture available, return the image URL
+                const imageData = await imageResponse.blob();
+                return URL.createObjectURL(imageData);
+            } else {
+                // If there's no picture available, return the URL of a default image
+                return 'house.png';
+            }
+        } catch (error) {
+            console.error('Error fetching apartment image:', error);
+            // Return the URL of a default image in case of an error
+            return 'house.png';
+        }
+    }
+
+    async displayApartmentDetails(apartmentDetails) {
+        try {
+            // Fetch the apartment image URL
+            const imageUrl = await this.fetchApartmentImage();
+
+            // Construct the query parameters string
+            const queryParams = new URLSearchParams({
+                location: apartmentDetails.location,
+                pricePerNight: apartmentDetails.pricePerNight,
+                availability: JSON.stringify(apartmentDetails.availability),
+                reviews: apartmentDetails.reviews,
+                avgRate: apartmentDetails.avgRate,
+                photo: imageUrl,
+                connectionDetails: apartmentDetails.connectionDetails
+            });
+
+            // Redirect to apartment.html with query parameters
+            window.location.href = `apartment.html?${queryParams.toString()}`;
+        } catch (error) {
+            console.error('Error displaying apartment details:', error);
+        }
     }
 }
