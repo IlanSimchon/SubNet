9313ca235dc475e79ab4230f70fb1e7879df068d
Ilan <smilan273@gmail.com>
2024-03-04 21:46:49 +0200

    change add apartment

--- src/house_owner.js
+++ src/house_owner.js
@@ -1,7 +1,11 @@
+const addApartmentFormContainer = document.getElementById('addApartmentFormContainer');
+
+
 class house_owner {
     constructor() {
         document.addEventListener('DOMContentLoaded', this.displayApartments.bind(this));
     }
+
 
     async displayApartments() {
         const userDataString = localStorage.getItem('userData');
@@ -102,119 +106,130 @@ function generateStarRating(averageRate) {
 
 }
 
-// Add an apartment
-document.addEventListener('DOMContentLoaded', () => {
-    const addApartmenDetailstBtn = document.getElementById('addApartmenDetailstBtn');
-    addApartmenDetailstBtn.addEventListener('click', openAddApartmentTab);
 
-    async function openAddApartmentTab() {
-        const addApartmentFormContainer = document.getElementById('addApartmentFormContainer');
-        addApartmentFormContainer.style.display = (addApartmentFormContainer.style.display === 'none') ? 'block' : 'none';
+async function openAddApartmentTab() {
+    if(addApartmentFormContainer.style.display === 'none'){
+        addApartmentFormContainer.style.display = 'block';
+    }
+    else{
+        addApartmentFormContainer.style.display = 'none';
+        return
+    }
 
-        const addApartmentForm = document.getElementById('addApartmentForm');
-        const successMessage = document.getElementById('successMessage');
-        const errorMessage = document.getElementById('errorMessage');
+    const addApartmentForm = document.getElementById('addApartmentForm');
+    const successMessage = document.getElementById('successMessage');
+    const errorMessage = document.getElementById('errorMessage');
 
-        // Additional elements
-        const apartmentDetailsContainer = document.getElementById('apartmentDetailsContainer');
-        const apartmentDetails = document.getElementById('apartmentDetails');
-        const confirmApartmentBtn = document.getElementById('confirmApartmentBtn');
+    // Additional elements
+    const apartmentDetailsContainer = document.getElementById('apartmentDetailsContainer');
+    const apartmentDetails = document.getElementById('apartmentDetails');
+    const confirmApartmentBtn = document.getElementById('confirmApartmentBtn');
 
-        addApartmentForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
+    addApartmentForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-            const locationInput = document.getElementById('locationInput');
-            const priceInput = document.getElementById('priceInput');
-            const startDateInput = document.getElementById('startDateInput');
-            const endDateInput = document.getElementById('endDateInput');
-            const photoInput = document.getElementById('photoInput');
-
-
-            const currentUserName = await getCurrentUser();
-            // Make a GET request to the getUser endpoint with the userName as a query parameter
-
-            let startD;
-            if (currentUserName) {
-                try {
-                    const response = await fetch(`http://localhost:63341/getUser?userName=${currentUserName}`);
-
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! Status: ${response.status}`);
-                    }
-                    const userData = await response.json();
-
-                    console.log(userData);
+        const locationInput = document.getElementById('locationInput');
+        const priceInput = document.getElementById('priceInput');
+        const startDateInput = document.getElementById('startDateInput');
+        const endDateInput = document.getElementById('endDateInput');
+        const photoInput = document.getElementById('photoInput');
 
 
-                    let startD = startDateInput.value;
-                    let endD = endDateInput.value
-                    let apartmentID = await AddApartment(
-                        locationInput.value,
-                        parseFloat(priceInput.value),
-                        {
-                            startDate: startD,
-                            endDate: endD
-                        },
-                        currentUserName,
-                        userData.email + ", " + userData.phone,
-                        [],
-                        0
-                    );
-                    console.log("apartmentID: " + apartmentID);
-                    console.log("photo path: " + photoInput.value); //todo: fix the photo path! ("fake path")
+        const currentUserName = await getCurrentUser();
+        // Make a GET request to the getUser endpoint with the userName as a query parameter
 
+        let startD;
+        if (currentUserName) {
+            try {
+                const response = await fetch(`http://localhost:63341/getUser?userName=${currentUserName}`);
 
-                    // Send POST request to addImageToApartment endpoint with apartment ID and image path
-                    const addImageResponse = await fetch('http://localhost:63341/addImageToApartment', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            apartmentId: apartmentID,
-                            imagePath: photoInput.value
-                        })
-                    });
-
-                    if (!addImageResponse.ok) {
-                        throw new Error('Failed to add image to apartment');
-                    }
-                    refreshPage();
-
-                } catch (error) {
-                    console.error('Error adding apartment:', error.message);
-
-                    // Display error message
-                    errorMessage.textContent = `Error adding apartment: ${error.message}`;
-                    successMessage.textContent = ''; // Clear any previous success message
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
                 }
-            } else {
-                console.error('Could not fetch current user details.');
+                const userData = await response.json();
+
+                console.log(userData);
+
+
+                let startD = startDateInput.value;
+                let endD = endDateInput.value
+                let apartmentID = await AddApartment(
+                    locationInput.value,
+                    parseFloat(priceInput.value),
+                    {
+                        startDate: startD,
+                        endDate: endD
+                    },
+                    currentUserName,
+                    userData.email + ", " + userData.phone,
+                    [],
+                    0
+                );
+                console.log("apartmentID: " + apartmentID);
+                console.log("photo path: " + photoInput.value); //todo: fix the photo path! ("fake path")
+
+
+                // Send POST request to addImageToApartment endpoint with apartment ID and image path
+                const addImageResponse = await fetch('http://localhost:63341/addImageToApartment', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        apartmentId: apartmentID,
+                        imagePath: photoInput.value
+                    })
+                });
+
+                if (!addImageResponse.ok) {
+                    throw new Error('Failed to add image to apartment');
+                }
+                refreshPage();
+
+            } catch (error) {
+                console.error('Error adding apartment:', error.message);
 
                 // Display error message
-                errorMessage.textContent = 'Could not fetch current user details.';
+                errorMessage.textContent = `Error adding apartment: ${error.message}`;
                 successMessage.textContent = ''; // Clear any previous success message
             }
-            // Display apartment details for confirmation
-            apartmentDetails.innerHTML = `
+        } else {
+            console.error('Could not fetch current user details.');
+
+            // Display error message
+            errorMessage.textContent = 'Could not fetch current user details.';
+            successMessage.textContent = ''; // Clear any previous success message
+        }
+        // Display apartment details for confirmation
+        apartmentDetails.innerHTML = `
                 <p><strong>Location:</strong> ${locationInput.value}</p>
                 <p><strong>Price Per Night:</strong> ${priceInput.value}</p>
                 <p><strong>Availability:</strong> ${startDateInput.value} to ${endDateInput.value}</p>
                 <!-- Add other details here -->
                 `;
 
-            // Show the apartment details container
-            addApartmentFormContainer.style.display = 'none';
-            apartmentDetailsContainer.style.display = 'block';
-        });
-        // Event listener for confirming apartment details
-        confirmApartmentBtn.addEventListener('click', async () => {
-            // ... (existing code for submitting the form)
+        // Show the apartment details container
+        addApartmentFormContainer.style.display = 'none';
+        apartmentDetailsContainer.style.display = 'block';
+    });
+    // Event listener for confirming apartment details
+    confirmApartmentBtn.addEventListener('click', async () => {
+        // ... (existing code for submitting the form)
 
-            // Hide the details container after confirmation
-            apartmentDetailsContainer.style.display = 'none';
-        });
-    }
+        // Hide the details container after confirmation
+        apartmentDetailsContainer.style.display = 'none';
+    });
+}
+
+// Add an apartment
+document.addEventListener('DOMContentLoaded', () => {
+    const addApartmenDetailstBtn = document.getElementById('addApartmenDetailstBtn');
+    addApartmenDetailstBtn.addEventListener('click', openAddApartmentTab);
+
+    const closeAddApartmentForm = document.getElementById('closeFormBtn');
+    closeAddApartmentForm.addEventListener('click', () => {
+        addApartmentFormContainer.style.display = 'none';
+    });
 
 
     // Function to get current username
